df0cc84944307c242fe3614a424d9e0c9c0ef72e
Aditya Sett <adityasett2004@gmail.com>
2025-09-05 11:36:37 +0530

    getCurrentLocation function added to use anywhere when needed ,,just call

--- app/src/main/java/com/mckv/attendance/utils/LocationHelper.kt
+++ app/src/main/java/com/mckv/attendance/utils/LocationHelper.kt
@@ -1,2 +1,20 @@
 package com.mckv.attendance.utils
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import com.google.android.gms.location.LocationServices
+
+@SuppressLint("MissingPermission") // request permission before using
+fun getCurrentLocation(context: Context, onLocationReceived: (Double, Double) -> Unit) {
+    val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+
+    fusedLocationClient.lastLocation
+        .addOnSuccessListener { location: Location? ->
+            if (location != null) {
+                onLocationReceived(location.latitude, location.longitude)
+            } else {
+                onLocationReceived(0.0, 0.0) // fallback
+            }
+        }
+}
