47d5a875e29c7d09113c88190a9c4d539b96c932
Aditya Sett <adityasett2004@gmail.com>
2025-09-14 11:12:32 +0530

    Reusable function for fetch location

--- app/src/main/java/com/mckv/attendance/utils/LocationHelper.kt
+++ app/src/main/java/com/mckv/attendance/utils/LocationHelper.kt
@@ -5,16 +5,37 @@ import android.content.Context
 import android.location.Location
 import com.google.android.gms.location.LocationServices
 
-@SuppressLint("MissingPermission") // request permission before using
-fun getCurrentLocation(context: Context, onLocationReceived: (Double, Double) -> Unit) {
+@SuppressLint("MissingPermission")
+fun getCurrentLocation(context: Context, onLocationFetched: (Double, Double) -> Unit) {
     val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
 
-    fusedLocationClient.lastLocation
-        .addOnSuccessListener { location: Location? ->
-            if (location != null) {
-                onLocationReceived(location.latitude, location.longitude)
-            } else {
-                onLocationReceived(0.0, 0.0) // fallback
+    try {
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    onLocationFetched(location.latitude, location.longitude)
+                } else {
+                    // fallback: request a new location
+                    val request = com.google.android.gms.location.LocationRequest.Builder(
+                        com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY, 1000
+                    ).setMaxUpdates(1).build()
+
+                    fusedLocationClient.requestLocationUpdates(
+                        request,
+                        object : com.google.android.gms.location.LocationCallback() {
+                            override fun onLocationResult(result: com.google.android.gms.location.LocationResult) {
+                                fusedLocationClient.removeLocationUpdates(this)
+                                val loc = result.lastLocation
+                                if (loc != null) {
+                                    onLocationFetched(loc.latitude, loc.longitude)
+                                }
+                            }
+                        },
+                        context.mainLooper
+                    )
+                }
             }
-        }
+    } catch (e: SecurityException) {
+        e.printStackTrace()
+    }
 }
